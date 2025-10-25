881ddc94c4b652c269a7140293aabf16fa1f08d9
Giang Pham <phamgiang2510@gmail.com>
2025-10-25 11:01:06 +0700

    [FIX] hr: Prevent fail on end-migration

--- openupgrade_scripts/scripts/hr/17.0.1.1/end-migration.py
+++ openupgrade_scripts/scripts/hr/17.0.1.1/end-migration.py
@@ -1,6 +1,12 @@
 # Copyright 2024 Viindoo Technology Joint Stock Company (Viindoo)
 # License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+import logging
+
 from openupgradelib import openupgrade
+
+from odoo.exceptions import UserError
+
+_logger = logging.getLogger(__name__)
 
 
 def _merge_employee_contact(env):
@@ -16,9 +22,17 @@ def _merge_employee_contact(env):
         """
     )
     for address_home_id, work_contact_id in env.cr.fetchall():
-        partner_merge_wizard._merge(
-            [address_home_id, work_contact_id], partner.browse(work_contact_id)
-        )
+        if address_home_id and work_contact_id and address_home_id != work_contact_id:
+            try:
+                partner_merge_wizard._merge(
+                    [address_home_id, work_contact_id], partner.browse(work_contact_id)
+                )
+            except UserError as e:
+                _logger.warning(
+                    f"Error merging employee contact {address_home_id} and "
+                    f"{work_contact_id}: {e}. Skipping this merge."
+                )
+                continue
 
 
 @openupgrade.migrate()
